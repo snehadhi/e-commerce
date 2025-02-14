088cf5cbdc207fabc660fe97d1b914ad60811722
snehadhi <138194206+snehadhi@users.noreply.github.com>
2025-02-14 15:08:03 +0530

    Updated axios.js

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-	baseURL: import.meta.mode === "development" ? "https://e-commerce-backend-9ghd.onrender.com" : "/api",
+	baseURL: import.meta.mode === "development" ? "https://e-commerce-backend-ulll.onrender.com" : "/api",
 	withCredentials: true, // send cookies to the server
 });
 
